0b6017ccdc1b9ed3033dc8d5674322e4e999fe93
Igor Pilarski <avgorpl@gmail.com>
2025-03-25 18:55:25 +0100

    register before checkout test added

--- cypress/e2e/tests/place-order-tests.cy.ts
+++ cypress/e2e/tests/place-order-tests.cy.ts
@@ -1,36 +1,62 @@
-import cartTable from "../components/cart-table-component";
-import navbar from "../components/navbar-component";
-import productsList from "../components/product-list-component";
-import cartPage from "../pages/cart-page";
-import checkoutPage from "../pages/checkout-page";
-import homePage from "../pages/home-page";
-import loginPage from "../pages/login-page";
+import cartTable from '../components/cart-table-component';
+import navbar from '../components/navbar-component';
+import productsList from '../components/product-list-component';
+import cartPage from '../pages/cart-page';
+import checkoutPage from '../pages/checkout-page';
+import homePage from '../pages/home-page';
+import loginPage from '../pages/login-page';
+import users from '../../fixtures/usersData.json';
 
 describe('Place Order tests', () => {
-    it(`Given: user is on home page
+  it(`Given: user is on home page
         When: user adds products to the cart
         And: proceeds to checkout
         And: registers a new account during checkout
         Then: user should be able to place the order successfully
         And: see a success message and delete the account afterwards`, () => {
-      cy.visit("");
-      homePage.verifyHomePageLoaded();
-      productsList.addFirstProductsToCart(3)
-      productsList.goToCartAfterAddingProduct();
-      cartPage.visitAndVerifyCartWithItemsLoaded();
-      cartPage.goToCheckout();
-      checkoutPage.goToLoginFromCheckout()
-      loginPage.registerRandomUser()
-      navbar.goTo('cartPage')
-      cartTable.saveCartItems()
-      cartPage.goToCheckout();
-      checkoutPage.verifyUserDetailsInCheckout("randomEmailUser")
-      checkoutPage.verifyReviewYourOrder()
-      cartTable.compareCheckoutWithSavedCartItems()
-      checkoutPage.completeCheckout()
-      checkoutPage.enterPaymentDetails() 
-      checkoutPage.confirmPaymentDetails()
-      checkoutPage.verifyOrderCompleted()
-      navbar.deleteCurrentUser()
-      })
-})
+    cy.visit('');
+    homePage.verifyHomePageLoaded();
+    productsList.addFirstProductsToCart(3);
+    productsList.goToCartAfterAddingProduct();
+    cartPage.visitAndVerifyCartWithItemsLoaded();
+    cartPage.goToCheckout();
+    checkoutPage.goToLoginFromCheckout();
+    loginPage.registerRandomUser();
+    navbar.goTo('cartPage');
+    cartTable.saveCartItems();
+    cartPage.goToCheckout();
+    checkoutPage.verifyUserDetailsInCheckout('randomEmailUser');
+    checkoutPage.verifyReviewYourOrder();
+    cartTable.compareCheckoutWithSavedCartItems();
+    checkoutPage.completeCheckout();
+    checkoutPage.enterPaymentDetails();
+    checkoutPage.confirmPaymentDetails();
+    checkoutPage.verifyOrderCompleted();
+    navbar.deleteCurrentUser();
+  });
+
+  it.only(`Given: user is on home page
+      When: user registers a new account
+      And: adds products to the cart
+      And: proceeds to checkout
+      Then: user should be able to place the order successfully
+      And: see a success message 
+      And: delete the account afterwards`, () => {
+    cy.visit('');
+    homePage.verifyHomePageLoaded();
+    navbar.goTo('loginPage');
+    loginPage.registerRandomUser();
+    productsList.addFirstProductsToCart(3);
+    navbar.goTo('cartPage');
+    cartPage.visitAndVerifyCartWithItemsLoaded();
+    cartTable.saveCartItems();
+    cartPage.goToCheckout();
+    checkoutPage.verifyUserDetailsInCheckout('randomEmailUser');
+    cartTable.compareCheckoutWithSavedCartItems();
+    checkoutPage.completeCheckout();
+    checkoutPage.enterPaymentDetails();
+    checkoutPage.confirmPaymentDetails();
+    checkoutPage.verifyOrderCompleted();
+    navbar.deleteCurrentUser();
+  });
+});
