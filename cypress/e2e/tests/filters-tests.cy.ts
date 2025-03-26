3003335d46388e92b356f3f431cde4ee11ac77bf
Igor Pilarski <avgorpl@gmail.com>
2025-03-26 21:23:14 +0100

    it.only deletion

--- cypress/e2e/tests/filters-tests.cy.ts
+++ cypress/e2e/tests/filters-tests.cy.ts
@@ -1,3 +1,4 @@
+import brandsComponent from '../components/brand-filter-component';
 import brands from '../components/brand-filter-component';
 import categories from '../components/category-filter-component';
 import navbar from '../components/navbar-component';
@@ -19,14 +20,14 @@ describe('Filters tests', () => {
     categories.enterTheCategory('Men', 'Jeans');
     filteredProductsPage.verifyFilteredProductsPageLoaded();
   });
-  it.only(`Given: user is on home page
+  it(`Given: user is on home page
     When: user navigates to Products page
     And: selects a brand from the sidebar
     Then: user should see brand-specific products
     And: can switch to another brand and see its products as well`, () => {
     cy.visit('');
     navbar.goTo('productsPage');
-    brands.verifyBrandsComponentIsDisplayed();
+    brandsComponent.verifyBrandsComponentIsDisplayed();
     brands.enterTheBrand('Polo');
     filteredProductsPage.verifyFilteredProductsPageLoaded();
     brands.enterTheBrand('Kookie Kids');
